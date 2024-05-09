e03dcd7e388c7685d844725db8ac98227f7778f6
Chahhat <chahhatp@gmail.com>
2024-05-08 21:49:16 -0700

    M13 Lab commit

---
+++ module01/src/employees.jsx
@@ -0,0 +1,21 @@
+let eren = "Eren Edgar";
+let lucy = "Lucy Heartfilia";
+let molly = "Molly Ho";
+let jimin = "Jimin Yuo";
+let mark = "Mark Dow";
+let jane = "Jane Dwe";
+let crystal = "Crystal Chow";
+let john = "John mark";
+const element = (
+  <ul style={{ color: "blue", fontSize: "24px" }}>
+    <li>{eren}</li>
+    <li>{lucy}</li>
+    <li>{molly}</li>
+    <li>{jimin}</li>
+    <li>{mark}</li>
+    <li>{jane}</li>
+    <li>{crystal}</li>
+    <li>{john}</li>
+  </ul>
+);
+ReactDOM.render(element, document.getElementById("content"));
